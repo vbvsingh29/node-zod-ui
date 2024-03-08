ed8149e8a94182d5f5fd6a2f46c842f69912e3b2
vbvsingh29 <vbvsingh2905@gmail.com>
2024-03-08 16:56:15 +0530

    Routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
-const App = () => <div>App</div>;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/auth/login";
+import RegisterPage from "./pages/auth/register";
+import Home from "./pages/auth/home";
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
